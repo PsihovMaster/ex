9a90b96b3dbefcc7059065b79d568d8f85a548c1
PsihovMaster <psihovtest@gmail.com>
2018-03-12 16:19:43 +0000

    test

---
+++ js/form.js
@@ -0,0 +1,33 @@
+$('#contact-form').submit(onContactFormSubmit);
+
+function onContactFormSubmit(event) {
+    event.preventDefault();
+
+    const name = $('#contact-form input[name=name]').val();
+    const email = $('#contact-form input[name=email]').val();
+    const note = $('#contact-form input[name=note]').val();
+    const data = {
+        name: name,
+        email: email,
+        note: note,
+    };
+
+    $('#loader').show();
+
+    $.ajax({
+        url: "https://tranquil-sierra-86540.herokuapp.com/yanisavchenko@gmail.com",
+        method: "POST",
+        data: data,
+        dataType: "json"
+    })
+        .done(function() {
+            $('#loader').hide();
+            $('#contact-form input[name=name]').val('');
+            $('#contact-form input[name=email]').val('');
+            $('#contact-form input[name=note]').val('');
+        })
+        .fail(function() {
+            $('#loader').hide();
+            alert('Error');
+        });
+}
